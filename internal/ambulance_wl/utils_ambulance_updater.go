9851439040df937edb8749ab2d09429ad5ce444b
borisce <borcer99@gmail.com>
2024-03-25 11:31:28 +0100

    ambulance waiting list first test

--- internal/ambulance_wl/utils_ambulance_updater.go
+++ internal/ambulance_wl/utils_ambulance_updater.go
@@ -4,7 +4,7 @@ import (
     "net/http"
 
     "github.com/gin-gonic/gin"
-    "github.com/<borisce>/ambulance-webapi/internal/db_service"
+    "github.com/borisce/ambulance-webapi/internal/db_service"
 )
 
 type ambulanceUpdater = func(
